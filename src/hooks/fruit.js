61d4e45c29ac737b6b31859dc0a8a6f791e445eb
alissin4444 <dev.alissonsantos@gmail.com>
2020-07-13 20:40:04 -0300

    finalized

--- src/hooks/fruit.js
+++ src/hooks/fruit.js
@@ -1,15 +1,27 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
+import AsyncStorage from '@react-native-community/async-storage';
+
 const FruitContext = createContext({});
 
 export const FruitProvider = ({ children }) => {
 
   const [fruits, setFruits] = useState([])
 
-  const addFruit = () => {
+  useEffect(() => {
+    AsyncStorage.getItem('@startkit_reactNative_navigation_asyncStorage/fruits').then(fruits => {
+      if(fruits) {
+        setFruits(JSON.parse(fruits)) 
+      }
+    })
+  }, [])
+
+
+  const addFruit = async () => {
     const fruitPosition = fruits.length + 1
     const fruit = `Fruit ${fruitPosition}`
     setFruits([...fruits, fruit])
+    await AsyncStorage.setItem('@startkit_reactNative_navigation_asyncStorage/fruits', JSON.stringify([...fruits, fruit]))
   }
 
   return (
